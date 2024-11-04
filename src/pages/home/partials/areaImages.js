889fc7506b31ab82baa868058951d0a2ca143a29
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-11-04 22:43:33 +0700

    update:area categories

---
+++ src/pages/home/partials/areaImages.js
@@ -0,0 +1,61 @@
+import american from '../../../../public/images/american.jpg';
+import italian from '../../../../public/images/italian.jpg';
+import british from '../../../../public/images/british.jpg';
+import canadian from '../../../../public/images/canadian.jpg';
+import chinese from '../../../../public/images/chinese.jpg';
+import croatian from '../../../../public/images/croatian.jpg';
+import dutch from '../../../../public/images/dutch.jpg';
+import egyptian from '../../../../public/images/egyptian.jpg';
+import filipino from '../../../../public/images/filipino.jpg';
+import french from '../../../../public/images/french.jpg';
+import greek from '../../../../public/images/greek.jpg';
+import indian from '../../../../public/images/indian.jpg';
+import irish from '../../../../public/images/irish.jpg';
+import jamaican from '../../../../public/images/jamaican.jpg';
+import japanese from '../../../../public/images/japanese.jpg';
+import kenyan from '../../../../public/images/kenyan.jpg';
+import malaysian from '../../../../public/images/malaysian.jpg';
+import mexican from '../../../../public/images/mexican.jpg';
+import moroccan from '../../../../public/images/moroccan.jpg';
+import polish from '../../../../public/images/polish.jpg';
+import portuguese from '../../../../public/images/portuguese.jpg';
+import russian from '../../../../public/images/russian.jpg';
+import spanish from '../../../../public/images/spanish.jpg';
+import thai from '../../../../public/images/thai.jpg';
+import tunisian from '../../../../public/images/tunisian.jpg';
+import turkish from '../../../../public/images/turkish.jpg';
+import ukrainian from '../../../../public/images/ukrainian.jpg';
+import vietnamese from '../../../../public/images/vietnamese.jpg';
+
+const areaImages = {
+  American: american,
+  British: british,
+  Canadian: canadian,
+  Chinese: chinese,
+  Croatian: croatian,
+  Dutch: dutch,
+  Egyptian: egyptian,
+  Filipino: filipino,
+  French: french,
+  Greek: greek,
+  Indian: indian,
+  Irish: irish,
+  Italian: italian,
+  Jamaican: jamaican,
+  Japanese: japanese,
+  Kenyan: kenyan,
+  Malaysian: malaysian,
+  Mexican: mexican,
+  Moroccan: moroccan,
+  Polish: polish,
+  Portuguese: portuguese,
+  Russian: russian,
+  Spanish: spanish,
+  Thai: thai,
+  Tunisian: tunisian,
+  Turkish: turkish,
+  Ukrainian: ukrainian,
+  Vietnamese: vietnamese,
+};
+
+export default areaImages;
